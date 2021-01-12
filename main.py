47412c14b355f06646b4f9dd120570ad7b0897ae
PippasSong <tlmag2108@gmail.com>
2021-01-12 18:38:09 +0900

    현재시각 가져오기, 종목명 가져오기, 오라클 접속, 로그, 오류 출력 메소드

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+import cx_Oracle
+
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
@@ -14,6 +17,7 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.g_scr_no = 0  # Open API 요청번호
 
         # 키움증권 클래스를 사용하기 위해 인스턴스 생성(ProgID를 사용)
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
@@ -33,7 +37,62 @@ class WindowClass(QMainWindow, form_class):
     def event_connect(self, err_code):
         if err_code == 0:
             self.text_edit.append("로그인 성공")
+            self.write_msg_log("로그인 성공")
 
+    # 현재시각 가져오기
+    def get_cur_tm(self):
+        time = QTime.currentTime()
+        answer = time.toString('hh.mm.ss')
+
+        return answer
+
+    # 종목명 가져오기. 종목코드(문자열)를 매개변수로 받음
+    def get_jongmok_nm(self, i_jongmok_cd):
+        jongmok_nm = self.kiwoom.dynamicCall("GetMasterCodeName(%s)" % i_jongmok_cd)  # 종목명 가져오기
+
+        return jongmok_nm
+
+    # 오라클 접속 연결 메소드
+    def connect_db(self):
+        dsn = cx_Oracle.makedsn("localhost", 1521, "xe")
+        db = cx_Oracle.connect("hr", "1234", dsn)
+
+        return db
+
+    # 로그 출력 메소드
+    def write_msg_log(self, text):
+        now = QDate.currentDate()
+        datetime = QDateTime.currentDateTime()
+        time = QTime.currentTime()
+        cur_time = datetime.toString()
+        cur_dt = now.toString(Qt.ISODate)
+        cur_tm = time.toString('hh.mm.ss')
+        cur_dtm = datetime.toString(Qt.DefaultLocaleShortDate)
+
+        self.textEdit.append(cur_dtm + " " + text)
+
+    # 오류 출력 메소드
+    def write_err_log(self, text):
+        now = QDate.currentDate()
+        datetime = QDateTime.currentDateTime()
+        time = QTime.currentTime()
+        cur_time = datetime.toString()
+        cur_dt = now.toString(Qt.ISODate)
+        cur_tm = time.toString('hh.mm.ss')
+        cur_dtm = datetime.toString(Qt.DefaultLocaleShortDate)
+
+        self.textEdit_2.append(cur_dtm + " " + text)
+
+    # 지연 메소드 time.sleep() 메소드
+
+    # 요청번호 부여 메소드
+    def get_scr_no(self):
+        if self.g_scr_no < 9999:
+            self.g_scr_no += 1
+        else:
+            self.g_scr_no = 1000
+
+        return str(self.g_scr_no)
 
 
 if __name__ == "__main__":
@@ -42,6 +101,7 @@ if __name__ == "__main__":
 
     # WindowClass의 인스턴스 생성
     myWindow = WindowClass()
+    myWindow.write_msg_log("asdf")
 
     # 프로그램 화면을 보여주는 코드
     myWindow.show()
